8732680223eabac96d8c45aec5ffb99ee338acbf
chubbah taught <33392685+taughta@users.noreply.github.com>
2018-08-01 10:20:25 +0200

    Update src.py

--- src.py
+++ src.py
@@ -1,34 +1,42 @@
-#!/bin/python
-
-import math
-import os
-import random
-import re
-import sys
-
 # Complete the minimumBribes function below.
 
-def minimumBribes(thisQueue):
-    if len(thisQueue) == 1:
+
+def calculate_if_too_chaotic(chaotoc_list):
+    people_that_moved = []
+    for ppl in range(1, len(chaotoc_list) + 1):
+        if not (ppl == chaotoc_list[ppl - 1]):
+            people_that_moved.append(ppl)
+
+    peoples_final_position = []
+    for mp in people_that_moved:
+        peoples_final_position.append(chaotoc_list.index(mp) + 1)
+
+    for z in range(len(people_that_moved)):
+        movement_value = people_that_moved[z] - peoples_final_position[z]
+        if movement_value > 2:
+            return "Too chaotic"
+
+    return "ok"
+
+
+def minimum_bribes(this_queue):
+    if len(this_queue) == 1:
         return 0
+
+    elif calculate_if_too_chaotic(this_queue) == "Too chaotic":
+            print("Too chaotic")
+
     else:
-        people_that_moved = []
-        for ppl in range(1,len(thisQueue)+1):
-            if not(ppl == thisQueue[ppl-1]):
-                people_that_moved.append(ppl)
-
-        peoples_final_position = []
-        for mp in people_that_moved:
-            peoples_final_position.append(thisQueue.index(mp) + 1)
-
         number_of_bribes = 0
-        for z in range(len(people_that_moved)):
-            movement_value = people_that_moved[z] - peoples_final_position[z]
-            if movement_value > 0:
-                number_of_bribes += movement_value
-            if movement_value > 2:
-                return "Too chaotic"
-        return number_of_bribes
+        for h in range(0, len(this_queue)-1):
+            for k in range(0, len(this_queue)-1):
+                if this_queue[k] > this_queue[k+1]:
+                    temp = this_queue[k]
+                    this_queue[k] = this_queue[k+1]
+                    this_queue[k+1] = temp
+                    number_of_bribes += 1
+
+        print(number_of_bribes)
 
 
 if __name__ == '__main__':
@@ -42,4 +50,4 @@ if __name__ == '__main__':
         q = list(map(int, input().rstrip().split()))
         assert len(q) == n, "The amount of entered number does not equal to the initially planned number of people."
 
-        print(minimumBribes(q))
+        minimum_bribes(q)
