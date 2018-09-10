fada7fb2b94acf0aeb75a8f16219110d3ab1cee1
chubbah taught <33392685+taughta@users.noreply.github.com>
2018-09-10 23:42:56 +0200

    Update src.py

--- src.py
+++ src.py
@@ -1,42 +1,37 @@
-# Complete the minimumBribes function below.
-
-
-def calculate_if_too_chaotic(chaotoc_list):
-    people_that_moved = []
-    for ppl in range(1, len(chaotoc_list) + 1):
-        if not (ppl == chaotoc_list[ppl - 1]):
-            people_that_moved.append(ppl)
-
-    peoples_final_position = []
-    for mp in people_that_moved:
-        peoples_final_position.append(chaotoc_list.index(mp) + 1)
-
-    for z in range(len(people_that_moved)):
-        movement_value = people_that_moved[z] - peoples_final_position[z]
-        if movement_value > 2:
-            return "Too chaotic"
-
-    return "ok"
-
-
 def minimum_bribes(this_queue):
-    if len(this_queue) == 1:
+
+    peoples_final_position = this_queue
+    nr_of_all_people = len(this_queue)
+    peoples_original_position = [z for z in range(1, nr_of_all_people+1)]
+
+    if nr_of_all_people == 1:
         return 0
-
-    elif calculate_if_too_chaotic(this_queue) == "Too chaotic":
-            print("Too chaotic")
-
     else:
-        number_of_bribes = 0
-        for h in range(0, len(this_queue)-1):
-            for k in range(0, len(this_queue)-1):
-                if this_queue[k] > this_queue[k+1]:
-                    temp = this_queue[k]
-                    this_queue[k] = this_queue[k+1]
-                    this_queue[k+1] = temp
-                    number_of_bribes += 1
+        # Create a dictionary where the number of movements can be logged
+        movement_log = {}
+        for i in range(1, nr_of_all_people+1):
+            movement_log[i] = 0
 
-        print(number_of_bribes)
+        # Counter for number of movements
+        nr_of_movements = 0
+
+        while True:
+            for s in range(nr_of_all_people-1, 0, -1):
+                if peoples_final_position[s] < peoples_final_position[s-1]:
+                    temp_a, temp_b = peoples_final_position[s], peoples_final_position[s-1]
+                    peoples_final_position[s], peoples_final_position[s-1] = temp_b, temp_a
+                    movement_log[peoples_final_position[s]] += 1
+                    nr_of_movements += 1
+                else:
+                    continue
+            if peoples_original_position == peoples_final_position:
+                break
+
+        # Check in the movement log dictionary if any of the people has more than 2 movements
+        for k in movement_log:
+            if movement_log.get(k) > 2:
+                return "Too chaotic"
+        return nr_of_movements
 
 
 if __name__ == '__main__':
@@ -50,4 +45,4 @@ if __name__ == '__main__':
         q = list(map(int, input().rstrip().split()))
         assert len(q) == n, "The amount of entered number does not equal to the initially planned number of people."
 
-        minimum_bribes(q)
+        print(minimum_bribes(q))
